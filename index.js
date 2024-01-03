37445112ee98933fd1a1cf3b7f13801cbada235d
Carlo-sh <cruaro@protonmail.com>
2024-01-03 17:35:20 +0100

    Changed plain constructor with classes

--- index.js
+++ index.js
@@ -10,16 +10,18 @@ const read = document.getElementById('read');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-	this.info = function () {
-		return `${this.title} by ${this.author}, ${this.pages} pages, ${
-			this.read ? 'already read' : 'not read yet'
-		}`;
-	};
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+		this.info = function () {
+			return `${this.title} by ${this.author}, ${this.pages} pages, ${
+				this.read ? 'already read' : 'not read yet'
+			}`;
+		};
+	}
 }
 
 function createNewCard(idx = myLibrary.length - 1) {
